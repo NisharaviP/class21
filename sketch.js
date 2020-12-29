d77e9a824c8272b961142d085892955d0d61c488
NisharaviP <75691604+NisharaviP@users.noreply.github.com>
2020-12-29 15:21:59 +0530

    Add files via upload
    c21

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var fixedRect, movingRect;
+var car,wall;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  car=createSprite(100,200,50,50);
+  car.shapeColor="blue";
+
+  wall=createSprite(600,200,50,100);
+  wall.shapeColor="yellow";
+  
+  car.velocityX=2;
+  
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(car,wall)){
+    textSize(50);
+  text("bang!!",400,400);
+car.shapeColor="red";
+
+  }
+
+  bounceOff(car,wall);
+  drawSprites();
+}
+
